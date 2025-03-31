b9b135a3cc6e261aa3c6e9fad0268525e7bfdb49
Harshita Khattar <125151939+hkhattar22@users.noreply.github.com>
2025-03-31 19:54:07 +0530

    loginSignupPage added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, withRouter } from "react-router-dom"; 
+import LoginSignupPage from "../src/pages/loginSignupPage/LoginSignup";
 import ToDoPage from "../src/pages/todo/Todo";
 import CommunityPage from "../src/pages/community/CommunityPage";
 import ResumeScorer from "../src/pages/resumeScorer/ResumeScorer";
@@ -9,7 +10,6 @@ import ChatBot from "../src/components/chatbot/ChatBot";
 import NotesPage from "../src/pages/notes/NotesPage";
 import ChatPage from "../src/pages/chat/ChatWithImage";
 import Dashboard from "../src/pages/dashboard/Dashboard";
-// import Brain from "../src/pages/Flashcards/FlashcardPage";
 import { ErrorProvider } from "./contexts/ErrorContext";
 import { TaskProvider } from "./contexts/TaskContext";
 import { NotesProvider } from "./contexts/NotesContext";
@@ -18,8 +18,11 @@ import { SongsProvider } from "./contexts/SongsContext";
 import Alert from "./components/alert/Alert";
 import Navbar from "./components/navbar/Navbar";
 
-function App() {
+function App({ location }) {
   const username = "NewUser";
+  const shouldShowSideNav = !['/', '/login-signup'].includes(location.pathname);
+  const noChatbotPaths = !['/', '/login-signup'].includes(location.pathname);
+
   return (
     <ErrorProvider>
       <TaskProvider>
@@ -28,10 +31,11 @@ function App() {
             <SongsProvider>
               <Router>
                 <div className="app-container">
-                  <Navbar />
+                  {shouldShowSideNav && <Navbar />}
                   <Alert />
                   <div className="content-container">
                     <Switch>
+                      <Route path="/login-signup" component={LoginSignupPage} />
                       <Route path="/todo" component={ToDoPage} />
                       <Route path="/community" render={(props) => <CommunityPage {...props} username={username} />} />
                       <Route path="/resume" component={ResumeScorer} />
@@ -39,9 +43,8 @@ function App() {
                       <Route path="/notes" component={NotesPage} />
                       <Route path="/chat" component={ChatPage} />
                       <Route path="/dashboard" component={Dashboard} />
-                      {/* <Route path="/brain" component={Brain} /> */}
                     </Switch>
-                    <ChatBot />
+                    {noChatbotPaths && <ChatBot />}
                   </div>
                 </div>
               </Router>
@@ -53,4 +56,5 @@ function App() {
   );
 }
 
-export default App;
+// Wrap App with withRouter to inject location as a prop
+export default withRouter(App);
