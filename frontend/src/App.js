3b91d1dd17abcac2365375794a3eafa9c3d61321
prishasawhney <prishasawhney03@gmail.com>
2025-04-02 01:51:11 +0530

    App.js structure changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LoginSignupPage from "../src/pages/LoginSignupPage/LoginSignup";
 import ToDoPage from "../src/pages/todo/Todo";
 import CommunityPage from "../src/pages/community/CommunityPage";
 import ResumeScorer from "../src/pages/resumeScorer/ResumeScorer";
@@ -9,45 +10,61 @@ import ChatBot from "../src/components/chatbot/ChatBot";
 import NotesPage from "../src/pages/notes/NotesPage";
 import ChatPage from "../src/pages/chat/ChatWithImage";
 import Dashboard from "../src/pages/dashboard/Dashboard";
-import Brain from "../src/pages/Flashcards/FlashcardPage";
+import Brain from "../src/pages/brain/BrainPage";
 import { ErrorProvider } from "./contexts/ErrorContext";
 import { TaskProvider } from "./contexts/TaskContext";
+import { TaskTypeProvider } from "./contexts/TaskTypeContext";
 import { NotesProvider } from "./contexts/NotesContext";
 import { PostProvider } from "./contexts/PostsContext";
 import { SongsProvider } from "./contexts/SongsContext";
+import { FlashcardProvider } from "./contexts/FlashcardContext";
 import Alert from "./components/alert/Alert";
 import Navbar from "./components/navbar/Navbar";
 
+function AppContent() {
+  const location = useLocation();
+  const username = "";
+
+  const shouldShowSideNav = !['/', '/login-signup'].includes(location.pathname);
+  const noChatbotPaths = !['/', '/login-signup'].includes(location.pathname);
+
+  return (
+    <div className="app-container">
+      {shouldShowSideNav && <Navbar />}
+      <Alert />
+      <div className="content-container">
+        <Routes>
+          <Route path="/login-signup" element={<LoginSignupPage />} />
+          <Route path="/todo" element={<ToDoPage />} />
+          <Route path="/community" element={<CommunityPage username={username} />} />
+          <Route path="/resume" element={<ResumeScorer />} />
+          <Route path="/interview" element={<InterviewAnalyzer />} />
+          <Route path="/notes" element={<NotesPage />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="/dashboard" element={<Dashboard username={username}/>} />
+          <Route path="/brain" element={<Brain/>} />
+        </Routes>
+        {noChatbotPaths && <ChatBot />}
+      </div>
+    </div>
+  );
+}
+
 function App() {
-  const username = "NewUser";
   return (
     <ErrorProvider>
       <TaskProvider>
+        <TaskTypeProvider>
         <NotesProvider>
           <PostProvider>
-            <SongsProvider>
+              <FlashcardProvider>
               <Router>
-                <div className="app-container">
-                  <Navbar />
-                  <Alert />
-                  <div className="content-container">
-                    <Switch>
-                      <Route path="/todo" component={ToDoPage} />
-                      <Route path="/community" render={(props) => <CommunityPage {...props} username={username} />} />
-                      <Route path="/resume" component={ResumeScorer} />
-                      <Route path="/interview" component={InterviewAnalyzer} />
-                      <Route path="/notes" component={NotesPage} />
-                      <Route path="/chat" component={ChatPage} />
-                      <Route path="/dashboard" component={Dashboard} />
-                      <Route path="/brain" component={Brain} />
-                    </Switch>
-                    <ChatBot />
-                  </div>
-                </div>
+                <AppContent />
               </Router>
-            </SongsProvider>
+              </FlashcardProvider>
           </PostProvider>
         </NotesProvider>
+        </TaskTypeProvider>
       </TaskProvider>
     </ErrorProvider>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from "./App";
+import { SongsProvider } from "./contexts/SongsContext"; // Import the provider
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <SongsProvider>
+            <App />
+    </SongsProvider>
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
