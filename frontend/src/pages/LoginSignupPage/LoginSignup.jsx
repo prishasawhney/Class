030377e2f740e03f80b5aac938515a487bd63106
prishasawhney <prishasawhney03@gmail.com>
2025-04-05 00:46:02 +0530

    Login-Signup Page fixed.

--- frontend/src/pages/LoginSignupPage/LoginSignup.jsx
+++ frontend/src/pages/LoginSignupPage/LoginSignup.jsx
@@ -25,12 +25,6 @@ const LoginSignup = () => {
       setTimeout(() => setShake(false), 500);
     }
   }, [error]);
-  useEffect(() => {
-    if (error) {
-      setShake(true);
-      setTimeout(() => setShake(false), 500);
-    }
-  }, [error]);
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -39,7 +33,7 @@ const LoginSignup = () => {
         email: loginEmail,
         password: loginPassword,
       });
-      loginUser(response.data.username); // Update username in context
+      loginUser(response.data.username);
       navigate("/dashboard");
       // Handle successful login, e.g., store token, redirect, etc.
     } catch (err) {
@@ -87,56 +81,7 @@ const LoginSignup = () => {
       .getElementById("overlayInner")
       .classList.remove("overlayInner-moveHalfRight");
     document.getElementById("signInForm").classList.remove("shiftRight");
-    document.getElementById("overlay").classList.add("overlay-moveHalfLeft");
-    document
-      .getElementById("overlayInner")
-      .classList.add("overlayInner-moveHalfRight");
-    document.getElementById("signInForm").classList.add("shiftRight");
-  };
-  
-  return (
-    <div className="container">
-      <div id="signInSignUpBox">
-        <div id="overlay">
-          <div id="overlayInner">
-            <div id="signUp">
-              <h1>Welcome Back!</h1>
-              <p>
-                To keep connected with us please login with your personal info.
-              </p>
-              <button onClick={moveSliderRight}>Sign In</button>
-            </div>
-            <div id="signIn">
-              <h1>Hello, Friend!</h1>
-              <p>Enter your personal details and start your journey with us.</p>
-              <button onClick={moveSliderLeft}>Sign Up</button>
-            </div>
-          </div>
-        </div>
-        <div id="forms">
-          <div id="signInForm" className="shiftRight">
-            <div className="holder">
-              <h1>Sign in</h1>
-              <div id="social-media-holder">
-                <button href="#" className="social-media-button">
-                  <box-icon type="logo" name="meta" color="#ffffff"></box-icon>
-                </button>
-                <button href="#" className="social-media-button">
-                  <box-icon
-                    name="google"
-                    type="logo"
-                    color="#ffffff"
-                  ></box-icon>
-                </button>
-                <button href="#" className="social-media-button">
-                  <box-icon
-                    type="logo"
-                    name="twitter"
-                    color="#ffffff"
-                  ></box-icon>
-                </button>
-              </div>
-              <p>or use your account</p>
+
     document.getElementById("overlay").classList.add("overlay-moveHalfRight");
     document
       .getElementById("overlayInner")
@@ -240,60 +185,6 @@ const LoginSignup = () => {
                 </button>
               </div>
               <p>or use your email for registration</p>
-              <form action="" method="POST" onSubmit={handleLogin}>
-                <div className="input1">
-                  <box-icon type="solid" name="user-circle"></box-icon>
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    value={loginEmail}
-                    name="email_id"
-                    onChange={(event) => setLoginEmail(event.target.value)}
-                    required
-                  />
-                </div>
-                <div className="input1">
-                  <box-icon type="solid" name="lock"></box-icon>
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    value={loginPassword}
-                    name="pass"
-                    onChange={(event) => setLoginPassword(event.target.value)}
-                    required
-                  />
-                </div>
-                <button type="submit" className={`signup-signin-button ${shake ? "shake" : ""}`}>
-                  Sign In
-                </button>
-                <a href="/forgotpass" id="forgot-password">Forgot your password?</a>
-                <p id="error">{error}</p>
-              </form>
-            </div>
-          </div>
-          <div id="signUpForm">
-            <div className="holder">
-              <h1>Create Account</h1>
-              <div id="social-media-holder">
-                <button href="#" className="social-media-button">
-                  <box-icon type="logo" name="meta" color="#ffffff"></box-icon>
-                </button>
-                <button href="#" className="social-media-button">
-                  <box-icon
-                    name="google"
-                    type="logo"
-                    color="#ffffff"
-                  ></box-icon>
-                </button>
-                <button href="#" className="social-media-button">
-                  <box-icon
-                    type="logo"
-                    name="twitter"
-                    color="#ffffff"
-                  ></box-icon>
-                </button>
-              </div>
-              <p>or use your email for registration</p>
 
               <form method="POST" action="" onSubmit={handleSignup}>
                 <div className="input1">
@@ -343,62 +234,6 @@ const LoginSignup = () => {
                   />
                 </div>
                 <button type="submit" className={`signup-signin-button ${shake ? "shake" : ""}`}>Sign Up</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div> 
-    </div>
-  );
-              <form method="POST" action="" onSubmit={handleSignup}>
-                <div className="input1">
-                  <box-icon type="solid" name="user-circle"></box-icon>
-                  <input
-                    type="text"
-                    placeholder="Name"
-                    value={signupUsername}
-                    name="setuname"
-                    onChange={(e) => setSignupUsername(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="input1">
-                  <box-icon type="solid" name="envelope"></box-icon>
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
-                    value={signupEmail}
-                    name="setemail_id"
-                    onChange={(e) => setSignupEmail(e.target.value)}
-                    required
-                  />
-                </div>
-                <div className="input1">
-                  <box-icon type="solid" name="lock"></box-icon>
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    name="setpass"
-                    value={signupPassword}
-                    onChange={(e) => setSignupPassword(e.target.value)}
-                    required
-                  />
-                </div>
-                <div class="input1">
-                  <box-icon type="solid" name="lock"></box-icon>
-                  <input
-                    className="cp"
-                    type="password"
-                    placeholder="Confirm Password"
-                    value={signupPassword2}
-                    name="setpass2"
-                    onChange={(e) => setSignupPassword2(e.target.value)}
-                    required
-                  />
-                </div>
-                <button type="submit" className={`signup-signin-button ${shake ? "shake" : ""}`}>Sign Up</button>
-                <p id="error">{error}</p>
               </form>
             </div>
           </div>
